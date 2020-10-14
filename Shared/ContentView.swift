ad9f7de5440dab28b03605fd10c6cf57f6db86be
joogps <joaogabrielps13@hotmail.com>
2020-10-14 16:43:50 -0300

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by João Gabriel Pozzobon dos Santos on 14/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/NeomestreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeomestreApp.swift
+//  Shared
+//
+//  Created by João Gabriel Pozzobon dos Santos on 14/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NeomestreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
